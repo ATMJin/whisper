8f920cf01f91bf0372c7c2e0303bf426bd7c829d
ATMJin <atmjin0912@protonmail.com>
2024-10-05 11:20:04 +0800

    import

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import whisper
+from whisper_transcribe import WhisperTranscribe
 
 # 選擇 model: https://github.com/openai/whisper#available-models-and-languages
 model = whisper.load_model("large-v3").to("cuda")  # large: 2.88G
